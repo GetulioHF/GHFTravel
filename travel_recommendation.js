6d7d3d040f5467ad2b27a440ce7f7e3178f1d446
GetulioHF <getulio@gfs.com.br>
2025-11-28 13:14:41 -0500

    commit

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -17,18 +17,12 @@ function searchCountry() {
       const country = data.countries.find((item) => item.name.toLowerCase() === input);
 
       if (country) {
-        // Show country name
-        // resultDiv.innerHTML += `<h2>${country.name}</h2>`;
-        // Show cities for that country
-        // resultDiv.innerHTML += `<h3>Cities:</h3>`;
         country.cities.forEach(city => {resultDiv.innerHTML += `
-            <div>
-                <h4>${city.name}</h4>
-                ${city.imageUrl}
-                <p>${city.description}</p>
-            </div>`});
+            <h4>${city.name}</h4>
+            ${city.imageUrl}
+            <p>${city.description}</p>
+        `});
       } else {
-//        <outresult>
         resultDiv.innerHTML = "Destination not found."
 //        </outresult>
       }
@@ -39,24 +33,3 @@ function searchCountry() {
     })
 }
 btnSearch.addEventListener("click", searchCountry)
-
-function generateReport() {
-  const numPatients = patients.length
-  const conditionsCount = {
-    Diabetes: 0,
-    Thyroid: 0,
-    "High Blood Pressure": 0,
-  }
-
-  for (const patient of patients) {
-    conditionsCount[patient.condition]++
-    genderConditionsCount[patient.gender][patient.condition]++
-  }
-
-  report.innerHTML = `Number of patients: ${numPatients}<br><br>`
-  report.innerHTML += `Conditions Breakdown:<br>`
-  for (const condition in conditionsCount) {
-    report.innerHTML += `${condition}: ${conditionsCount[condition]}<br>`
-  }
-
-}
